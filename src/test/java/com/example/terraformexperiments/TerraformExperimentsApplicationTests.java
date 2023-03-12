8be373ca482ebb310070b99155754770a31c935f
Mike Nielsen <mmni@protonmail.com>
2023-03-12 21:06:04 +0100

    Initial commit

---
+++ src/test/java/com/example/terraformexperiments/TerraformExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.terraformexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
